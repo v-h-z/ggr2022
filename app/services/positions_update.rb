1a8f24f33f8bc0d02fa2f49b8b58b2ad769c7f00
Victor Holl <v_h@me.com>
2022-09-11 15:07:28 +0200

    wip yb api parsing

--- app/services/positions_update.rb
+++ app/services/positions_update.rb
@@ -15,6 +15,21 @@ class PositionsUpdate
   def fetch_xml
     url = "https://cf.yb.tl/xml/#{@race.code}"
     positions_serialized = URI.open(url).read
-    p positions_xml = Nokogiri::XML(positions_serialized)
+    positions_xml = Nokogiri::XML(positions_serialized)
+    positions_xml.root.xpath("//Group/*").each do |data|
+      p data.attribute('name').text
+      p data.attribute('serial').text
+      p data.attribute('marker').text
+      data.children.each do |child|
+        child.children.each do |grand_child|
+          p grand_child.name.strip
+          p grand_child.text.strip
+        end
+      end
+      break
+      # first_name = beatle.xpath("first_name").text
+      # last_name = beatle.xpath("last_name").text
+      # instrument = beatle.xpath("instrument").text
+    end
   end
 end
